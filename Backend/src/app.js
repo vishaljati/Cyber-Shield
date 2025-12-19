d4880c7d767c7c0f4e96485e1202c9fdfc918c8c
vishaljati <jativishal120@gmail.com>
2025-12-19 18:54:40 +0530

    Refactor backend structure: add app.js, update package.json, and remove TypeScript files

---
+++ Backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.REACT_CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+
+
+
+export default app;
