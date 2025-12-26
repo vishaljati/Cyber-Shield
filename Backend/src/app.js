ac85ea52d66e7ba0704d6caee0ab15f2031326aa
vishaljati <jativishal120@gmail.com>
2025-12-27 00:13:03 +0530

    Add cors origin for chrome extension

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.REACT_CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.EXTENSION_CLIENT_URL || 'http://localhost:5173',
     credentials: true,
   })
 );
